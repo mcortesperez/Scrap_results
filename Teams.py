ad1db507c810f1f632e3e3a10af7586907684ece
Mateo Cortes <tomic.ero1@gmail.com>
2023-02-10 12:51:23 -0500

    Teams_corrección1_plantilla_inicial

--- Teams.py
+++ Teams.py
@@ -38,60 +38,95 @@ sleep(2)
 Team = driver.find_element(By.XPATH, '//div[@class = "heading__name"]').text
 results = driver.find_elements(By.XPATH, '//div[@title = "¡Haga click para detalles del partido!"]')
 
-more_1matches = 0
-less_1matches = 0
+print('\n\n\t\t\t'+Team+'\n')
 
-more_2matches = 0
-less_2matches = 0
+Total_goals = []
 
-more_3matches = 0
-less_3matches = 0
-
-gandg_matches = 0
-no_gandg = 0
+cont_gg = 0
+cont_NOgg = 0
 
 for result in results:
     
     home_goals = int(result.find_element(By.XPATH, './/div[@class = "event__score event__score--home"]').text)
     away_goals = int(result.find_element(By.XPATH, './/div[@class = "event__score event__score--away"]').text)
     total_goals = home_goals + away_goals
+    Total_goals.append(total_goals)
     
-    if (home_goals != 0) and (away_goals != 0):
-        gandg_matches += 1
+    if (home_goals!=0 and away_goals!=0):
+        cont_gg += 1
     else:
-        no_gandg += 1
-    
-    if total_goals > 1.5:
-        more_1matches += 1
-    else:
-        less_1matches += 1
-    
-    if total_goals > 2.5:
-        more_2matches += 1
-    else:
-        less_2matches += 1
+        cont_NOgg += 1
         
-    if total_goals > 3.5:
-        more_3matches += 1
+prob_gg_result = round((cont_gg/len(results))*100, 2)
+prob_NOgg_result = round((cont_NOgg/len(results))*100, 2)
+
+print(f'Ambos marcan = {prob_gg_result}%\n(NO) ambos marcan = {prob_NOgg_result}%')
+    
+goals_limit = 0
+    
+while goals_limit < 5.5:
+    more = 0
+    less = 0
+    for x in Total_goals:
+        if x > goals_limit:
+            more += 1
+        else:
+            less += 1
+            
+    prob_more_result = round((more/len(Total_goals))*100, 2)
+    prob_less_result = round((less/len(Total_goals))*100, 2)
+
+    print('-------------------------------------------------------------------------')
+    print(f'mas de {goals_limit} = {prob_more_result}%\nmenos de {goals_limit} = {prob_less_result}%\n')
+    goals_limit += 0.5
+    
+    
+    
+driver.get('')
+
+sleep(1)
+
+Team = driver.find_element(By.XPATH, '//div[@class = "heading__name"]').text
+results = driver.find_elements(By.XPATH, '//div[@title = "¡Haga click para detalles del partido!"]')
+
+print('\n\n\t\t\t'+Team+'\n')
+
+Total_goals = []
+
+cont_gg = 0
+cont_NOgg = 0
+
+for result in results:
+    
+    home_goals = int(result.find_element(By.XPATH, './/div[@class = "event__score event__score--home"]').text)
+    away_goals = int(result.find_element(By.XPATH, './/div[@class = "event__score event__score--away"]').text)
+    total_goals = home_goals + away_goals
+    Total_goals.append(total_goals)
+    
+    if (home_goals!=0 and away_goals!=0):
+        cont_gg += 1
     else:
-        less_3matches += 1
+        cont_NOgg += 1
         
-prob_more1 = round((more_1matches/len(results))*100, 2)
-prob_less1 = round((less_1matches/len(results))*100, 2)
+prob_gg_result = round((cont_gg/len(results))*100, 2)
+prob_NOgg_result = round((cont_NOgg/len(results))*100, 2)
 
-prob_more2 = round((more_2matches/len(results))*100, 2)
-prob_less2 = round((less_2matches/len(results))*100, 2)
+print(f'Ambos marcan = {prob_gg_result}%\n(NO) ambos marcan = {prob_NOgg_result}%')
+    
+goals_limit = 0
+    
+while goals_limit < 5.5:
+    more = 0
+    less = 0
+    for x in Total_goals:
+        if x > goals_limit:
+            more += 1
+        else:
+            less += 1
+            
+    prob_more_result = round((more/len(Total_goals))*100, 2)
+    prob_less_result = round((less/len(Total_goals))*100, 2)
 
-prob_more3 = round((more_3matches/len(results))*100, 2)
-prob_less3 = round((less_3matches/len(results))*100, 2)
-
-prob_goal_and_goal = round((gandg_matches/len(results))*100, 2)
-prob_no_gandg = round((no_gandg/len(results))*100, 2)
-
-print('----------------------------------------------------------------------------------------\n')
-print(Team+'\n')
-print(f'mas de 1.5 = {prob_more1}%\nmenos de 1.5 = {prob_less1}%\n')
-print(f'mas de 2.5 = {prob_more2}%\nmenos de 2.5 = {prob_less2}%\n')
-print(f'mas de 3.5 = {prob_more3}%\nmenos de 3.5 = {prob_less3}%\n')
-print(f'ambos marcan = {prob_goal_and_goal}%\nNO marcan ambos = {prob_no_gandg}%\n')
-print('----------------------------------------------------------------------------------------')
+    print('-------------------------------------------------------------------------')
+    print(f'mas de {goals_limit} = {prob_more_result}%\nmenos de {goals_limit} = {prob_less_result}%\n')
+    goals_limit += 0.5
